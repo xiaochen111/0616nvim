007fcec62a77fc7f9a68d7b566e734a2cc7902c7
chenhaibo <chb0413@163.com>
2024-07-14 21:30:27 +0800

    fix: 添加花括号自动换行并格式化插件
    fix: ~

---
+++ lua/plugins/autoclose.lua
@@ -0,0 +1,14 @@
+
+
+---@type LazySpec
+return {
+  "m4xshen/autoclose.nvim",
+  opts = {
+      -- mappings = {
+      --   n = {
+      --     ["f"] = { "<cmd>HopChar1<CR>", desc = "search" },
+      --   },
+      -- },
+      --
+  },
+}
