6e2451abdbc76df9c1a9d139efa80b0172bfd3a5
chenhaibo <chb0413@163.com>
2026-06-09 17:58:43 +0800

    新增 gI 跳转到实现

--- lua/plugins/_astrolsp.lua
+++ lua/plugins/_astrolsp.lua
@@ -30,6 +30,9 @@ return {
         -- 没想这里可以把AstroNvim的默认gd覆盖掉 那么我先把我原来的gdd给注释掉
         ["gd"] = {
            require("telescope.builtin").lsp_references
+        },
+        ["gI"] = {
+           vim.lsp.buf.implementation
         }
       }
     }
